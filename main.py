de3bab6d2ee4b36db80499ccf0357011361094db
ryushade <77291154@usat.pe>
2024-06-04 12:08:51 -0500

    prueba

--- main.py
+++ main.py
@@ -477,7 +477,7 @@ def agregar_carrito():
 @app.route("/detalle_producto_categoria")
 def formulario_detalle_categoria():
     productosm = controlador_producto.obtener_moto_producto()
-    return render_template("categorias.html" , productosm=productosm)
+    return render_template("categoriasresp.html" , productosm=productosm)
 
 
 
